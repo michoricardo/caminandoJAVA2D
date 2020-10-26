14a232c676a58737aa32de42c4077fdf68be2b78
Ricardo <ricardog94@hotmail.com>
2020-10-26 12:43:27 -0600

    primera versión por si me equivoco

---
+++ src/mover_imagen/Contenedor.java
@@ -0,0 +1,137 @@
+
+package mover_imagen;
+
+
+  
+
+import javax.swing.JComponent;
+
+import javax.swing.JPanel;
+
+import javax.swing.ImageIcon;
+
+import java.awt.Graphics;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.BasicStroke;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
+
+public class Contenedor extends JComponent 
+{
+     static JPanel panel;
+    
+     static int columna = 3;
+     
+     static int fila = 195;
+     
+     static int numero = 1;
+    
+  Contenedor(JPanel panel)
+  {
+      this.panel = panel;
+      
+      setBounds(0, 0,panel.getWidth() , panel.getHeight());
+         
+  }
+    
+  public void paint(Graphics g)
+  {
+    ImageIcon imagen =new ImageIcon(new ImageIcon(getClass().getResource("imagenes/"+numero+".png")).getImage());
+    
+    g.setColor(Color.GRAY); //Color de la calle
+    g.fillRect(0, 500, 1000, 255); //x,y,width, height
+    
+    
+    g.setColor(Color.WHITE);
+    //líneas punteadas
+    Graphics2D g2d = (Graphics2D) g.create();
+    Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{40}, 0); //40 de espacio
+    g2d.setStroke(dashed);
+    g2d.drawLine(0, 540, 1000, 540); //x1,y1,x2,y2
+    //g2d.dispose();
+    
+    //Graphics2D g2d = (Graphics2D) g;
+    g2d.setColor(Color.RED);
+    g2d.fill(new Rectangle2D.Double(300, 400, 400, 100));//X,Y, WIDTH,,HEIGTH
+    g2d.fill(new Rectangle2D.Double(150, 75, 100, 50));
+    g2d.fill(new Polygon(new int[] {185, 150, 215}, new int[] {150, 200, 200}, 3));
+
+    g2d.dispose();
+    
+
+    
+    
+
+     
+     g.drawImage(imagen.getImage(), columna, fila, 206, 356, null); 
+     
+       g.setColor(Color.WHITE); //NUBES
+       
+       g.fillOval(35,40 ,100, 30);
+       g.fillOval(70,45 ,150, 50);
+       g.fillOval(160,55 ,100, 30);
+       g.fillOval(320,45 ,100, 25);
+       g.fillOval(340,40 ,100, 25);
+       g.fillOval(700,40 ,100, 30);
+       g.fillOval(780,40 ,100, 30);
+       g.fillOval(610,50 ,150, 30);
+       
+       g.setColor(Color.YELLOW); //SOL   
+       g.fillOval(480,30 ,80,80);
+       
+
+
+       
+       }
+  
+  static Thread hilo = new Thread()
+    {
+       @Override
+        public void run()
+        {
+            try
+            {
+                while(true)
+                {
+                  numero++;
+                  
+                  if(numero==5) //SON 5 IMAGENES, SE REPITE SI SE LLEGA A LA 5
+                  {
+                    numero=1;
+                  }
+                  
+                 panel.repaint();  
+                 
+                 columna+=10;
+                 
+                  hilo.sleep(100);
+                }
+                
+            } catch (java.lang.InterruptedException ex) {
+                                                           System.out.println(ex.getMessage()); 
+                                                         }
+        }
+    };
+   
+   public static void mover()
+  {
+     if(!hilo.isAlive())
+    {
+      hilo.start();
+    }
+      columna = 3;
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/mover_imagen/Ventana.java
@@ -0,0 +1,81 @@
+
+package mover_imagen;
+
+
+public class Ventana extends javax.swing.JFrame {
+
+    
+    public Ventana() 
+    {
+        initComponents();
+        
+        panel.add(new Contenedor(panel));
+        
+     }
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        boton = new javax.swing.JButton();
+        panel = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(205, 255, 255));
+
+        boton.setText("Activar");
+        boton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonActionPerformed(evt);
+            }
+        });
+
+        panel.setBackground(new java.awt.Color(205, 255, 255));
+
+        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
+        panel.setLayout(panelLayout);
+        panelLayout.setHorizontalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panelLayout.setVerticalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 588, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(boton)
+                .addGap(0, 852, Short.MAX_VALUE))
+            .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(boton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void botonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonActionPerformed
+        Contenedor.mover();
+    }//GEN-LAST:event_botonActionPerformed
+
+    public static void main(String args[]) {
+        
+        new Ventana().setVisible(true);
+            
+    }
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton boton;
+    private javax.swing.JPanel panel;
+    // End of variables declaration//GEN-END:variables
+}
